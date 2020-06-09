bffc255fce2512b5091440d124deb206c477ef11
Kaylie V <k.verner37@gmail.com>
2020-06-08 20:06:38 -0400

    pseudocode

---
+++ public/js/bidEditor.js
@@ -0,0 +1,5 @@
+ //get job post by id to edit bid amount
+
+ //update bid amount in database, route back to homepage
+
+  //delete bid, remove bid from database
---
+++ public/js/postEditor.js
@@ -0,0 +1,5 @@
+// get job post by id 
+
+// update post title, timeframe, and/or description
+
+//save button should save to database and route back to users.html
