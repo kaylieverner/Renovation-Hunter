4873b693ff46aa874f5c0dae27f55f05ceb6419d
Drew Swinicki <dswinicki93@gmail.com>
2020-06-11 21:19:04 -0400

    fixed login

--- public/js/signupuser.js
+++ public/js/signupuser.js
@@ -64,7 +64,7 @@ $(document).ready(function() {
 
       .then(function(data) {
         console.log(data);
-        window.location.replace('/members');
+        window.location.replace('/login');
       })
       .catch(handleLoginErr);
   }
